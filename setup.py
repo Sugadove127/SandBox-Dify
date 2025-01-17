d56924ab0b459c5cb42c485edf894df69bcce410
Sugadove127 <sugadove1997@qq.com>
2025-01-17 09:40:28 +0000

    Add setup.py or pyproject.toml

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='add_numbers',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 在这里添加项目的依赖包
+        
+    ]
+)
